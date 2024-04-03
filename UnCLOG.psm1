cca6b6f7e64e582d76c02aea6e0c63920a2debea
Mia <152985731+mia-kiwi@users.noreply.github.com>
2024-04-03 11:59:49 +0200

    UnCLOG24.0.5
    + Write-UnCLog: Directory creator if it doesn't exist
    + Write-UnCLog: Parameter Type now also accepts type "Success"
    + Write-UnCSuccess
    + Write-UnCInfo
    + Write-UnCWarning
    + Write-UnCError
    + Write-UnCDebug

--- UnCLOG.psm1
+++ UnCLOG.psm1
@@ -4,35 +4,67 @@
 #    Department: Department Of Information Technology                               #
 #    Division: Division Of Programming And Development                              #
 #    Group: Internal Tools Development Group                                        #
-#    Team: Powershell Development Team                                              #
+#    Team: PowerShell Development Team                                              #
 #                                                                                   #
 #    Level: 0                                                                       #
 #    Classification: PUBLIC                                                         #
-#    Version: 24.0.4                                                                #
+#    Version: 24.0.5                                                                #
 #                                                                                   #
 #    Name: Universal Computer Logging And Operational Guidelines                    #
 #    Description:                                                                   #
 #    Language: PowerShell                                                           #
 #    Contributor(s): DELANDM002                                                     #
 #    Created: 2024-03-29                                                            #
-#    Updated: 2024-04-02                                                            #
+#    Updated: 2024-04-03                                                            #
 #                                                                                   #
-#    SNAF: [UnCLOG24.0.4 ¦ LEVEL-0] - Universal Computer Logging and Operational    #
+#    SNAF: [UnCLOG24.0.5 ¦ LEVEL-0] - Universal Computer Logging and Operational    #
 #          Guidelines                                                               #
-#    DRL: DRL://afs/it/dpd/itdg/pdt#24.0.4                                          #
+#    DRL: DRL://afs/it/dpd/itdg/pdt#24.0.5                                          #
 #    DID: UDIS-0000000000000000000Z                                                 #
 #    Location: https://github.com/mia-kiwi/UnCLOG/                                  #
 #                                                                                   #
 #                                                                                   #
 #####################################################################################
 
+<#
+.SYNOPSIS
+Logs information about a specific event to a file.
+
+.DESCRIPTION
+This function logs information about a specific event to a file. The log file is created in the specified directory, with the name of the file being the date of the event. The log file is a JSON object.
+
+.PARAMETER Identifier
+The unique identifier of the log entry. If not specified, a new GUID is generated.
+
+.PARAMETER Type
+The type of the log entry. The default value is "Information".
+
+.PARAMETER Severity
+The severity of the log entry. The default value is "None".
+
+.PARAMETER AppName
+The name of the application that generated the log entry.
+
+.PARAMETER Head
+The title of the log entry.
+
+.PARAMETER Body
+The message of the log entry.
+
+.PARAMETER Directory
+The directory where the log file is created. The default value is "C:\Temp\UnCLOGS\".
+
+.EXAMPLE
+PS C:\> Write-UnCLog -Head "Test Log" -Body "This is a test log" -AppName "UnCLOG Tester" -Type Information -Severity Low
+PS C:\> Write-UnCLog -Head "Test Log" -Body "This is a test log" -AppName "UnCLOG Tester" -Type Information -Severity Low -Directory "C:\Temp\UnCLOGS\AppName1"
+#>
 function Write-UnCLog {
     param(
         [parameter()]
         [string] $Identifier = ([System.Guid]::newguid()).ToString(),
 
         [parameter()]
-        [ValidateSet("Information", "Warning", "Error", "Debug")]
+        [ValidateSet("Success", "Information", "Warning", "Error", "Debug")]
         [string] $Type = "Information",
 
         [parameter()]
@@ -40,7 +72,7 @@ function Write-UnCLog {
         [string] $Severity = "None",
 
         [parameter()]
-        [Alias("Application","App","Name")]
+        [Alias("Application", "App", "Name")]
         [string] $AppName = "",
 
         [parameter(Mandatory = $true, Position = 0)]
@@ -49,11 +81,11 @@ function Write-UnCLog {
 
         [parameter(Position = 1)]
         [Alias("Message", "Long")]
-        [string] $Body,
+        [object] $Body,
 
         [parameter()]
         [Alias("Location", "Folder", "Path", "Dir")]
-        [string] $Directory = 'C:\UnCLOGS\'
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
     )
 
     # Get and format the call stack
@@ -80,6 +112,15 @@ function Write-UnCLog {
         CallStack   = $CallStack
     }
 
+    # /!\ This block takes slows the function by around 10ms, but it's only executed once per log directory /!\
+    # Attempt to create the directory (even if it already exists, checking takes too long)
+    try {
+        [System.IO.Directory]::CreateDirectory($Directory) | Out-Null
+    }
+    catch {
+        Write-Warning "Failed to create the log directory: $_"
+    }
+
     try {
         $StreamWriter = [System.IO.StreamWriter]::New("$Directory\$($Log.DateTime.Substring(0,10)).unclog", $true)
         $StreamWriter.WriteLine([Newtonsoft.Json.JsonConvert]::SerializeObject($Log))
@@ -93,4 +134,151 @@ function Write-UnCLog {
     }
 
     return $null
+}
+
+function Write-UnCInfo {
+    param(
+        [parameter()]
+        [string] $Identifier = ([System.Guid]::newguid()).ToString(),
+
+        [parameter()]
+        [ValidateSet("None", "Low", "Medium", "High", "Critical")]
+        [string] $Severity = "None",
+
+        [parameter()]
+        [Alias("Application", "App", "Name")]
+        [string] $AppName = "",
+
+        [parameter(Mandatory = $true, Position = 0)]
+        [Alias("Title", "Short")]
+        [string] $Head,
+
+        [parameter(Position = 1)]
+        [Alias("Message", "Long")]
+        [object] $Body,
+
+        [parameter()]
+        [Alias("Location", "Folder", "Path", "Dir")]
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
+    )
+
+    Write-UnCLog -Identifier $Identifier -Severity $Severity -AppName $AppName -Head $Head -Body $Body -Directory $Directory -Type Information
+}
+
+function Write-UnCSuccess {
+    param(
+        [parameter()]
+        [string] $Identifier = ([System.Guid]::newguid()).ToString(),
+
+        [parameter()]
+        [ValidateSet("None", "Low", "Medium", "High", "Critical")]
+        [string] $Severity = "None",
+
+        [parameter()]
+        [Alias("Application", "App", "Name")]
+        [string] $AppName = "",
+
+        [parameter(Mandatory = $true, Position = 0)]
+        [Alias("Title", "Short")]
+        [string] $Head,
+
+        [parameter(Position = 1)]
+        [Alias("Message", "Long")]
+        [object] $Body,
+
+        [parameter()]
+        [Alias("Location", "Folder", "Path", "Dir")]
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
+    )
+
+    Write-UnCLog -Identifier $Identifier -Severity $Severity -AppName $AppName -Head $Head -Body $Body -Directory $Directory -Type Success
+}
+
+function Write-UnCWarning {
+    param(
+        [parameter()]
+        [string] $Identifier = ([System.Guid]::newguid()).ToString(),
+
+        [parameter()]
+        [ValidateSet("None", "Low", "Medium", "High", "Critical")]
+        [string] $Severity = "None",
+
+        [parameter()]
+        [Alias("Application", "App", "Name")]
+        [string] $AppName = "",
+
+        [parameter(Mandatory = $true, Position = 0)]
+        [Alias("Title", "Short")]
+        [string] $Head,
+
+        [parameter(Position = 1)]
+        [Alias("Message", "Long")]
+        [object] $Body,
+
+        [parameter()]
+        [Alias("Location", "Folder", "Path", "Dir")]
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
+    )
+
+    Write-UnCLog -Identifier $Identifier -Severity $Severity -AppName $AppName -Head $Head -Body $Body -Directory $Directory -Type Warning
+}
+
+
+function Write-UnCError {
+    param(
+        [parameter()]
+        [string] $Identifier = ([System.Guid]::newguid()).ToString(),
+
+        [parameter()]
+        [ValidateSet("None", "Low", "Medium", "High", "Critical")]
+        [string] $Severity = "None",
+
+        [parameter()]
+        [Alias("Application", "App", "Name")]
+        [string] $AppName = "",
+
+        [parameter(Mandatory = $true, Position = 0)]
+        [Alias("Title", "Short")]
+        [string] $Head,
+
+        [parameter(Position = 1)]
+        [Alias("Message", "Long")]
+        [object] $Body,
+
+        [parameter()]
+        [Alias("Location", "Folder", "Path", "Dir")]
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
+    )
+
+    Write-UnCLog -Identifier $Identifier -Severity $Severity -AppName $AppName -Head $Head -Body $Body -Directory $Directory -Type Error
+}
+
+
+function Write-UnCDebug {
+    param(
+        [parameter()]
+        [string] $Identifier = ([System.Guid]::newguid()).ToString(),
+
+        [parameter()]
+        [ValidateSet("None", "Low", "Medium", "High", "Critical")]
+        [string] $Severity = "None",
+
+        [parameter()]
+        [Alias("Application", "App", "Name")]
+        [string] $AppName = "",
+
+        [parameter(Mandatory = $true, Position = 0)]
+        [Alias("Title", "Short")]
+        [string] $Head,
+
+        [parameter(Position = 1)]
+        [Alias("Message", "Long")]
+        [object] $Body,
+
+        [parameter()]
+        [Alias("Location", "Folder", "Path", "Dir")]
+        [string] $Directory = 'C:\Temp\UnCLOGS\'
+    )
+
+    Write-UnCLog -Identifier $Identifier -Severity $Severity -AppName $AppName -Head $Head -Body $Body -Directory $Directory -Type Debug
 }
